c8f0fd77c5c313195080958050cfaea5e498244e
JulekKumpel <109910235+JulekKumpel@users.noreply.github.com>
2025-10-31 20:40:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:8081", "http://localhost:8082", "http://localhost:8083", "http://localhost:8084", "vraleague.netlify.app"],
+    origin: ["http://localhost:8081", "http://localhost:8082", "http://localhost:8083", "http://localhost:8084", "vra-league.netlify.app"],
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
@@ -154,3 +154,4 @@ io.on('connection', (socket) => {
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
